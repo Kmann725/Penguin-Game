00a4f2e5716b9fda7a71abf12dcf059d6c1c3844
Kmann725 <khmanning@mail.bradley.edu>
2023-02-27 11:59:49 -0600

    snowballs shrink on ice

--- Penguins/Assets/Scripts/Snowball.cs
+++ Penguins/Assets/Scripts/Snowball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Snowball : MonoBehaviour
 {
     public float maxSize = 6f;
+    public float minSize = 0.4f;
 
     private Rigidbody rb;
 
@@ -18,6 +19,10 @@ public class Snowball : MonoBehaviour
         if ((rb.velocity.x >= 0.15f || rb.velocity.x <= -0.15f) && (rb.velocity.z >= 0.15f || rb.velocity.z <= -0.15f) && collision.gameObject.CompareTag("GroundSnow") && transform.localScale.x <= maxSize)
         {
             transform.localScale = transform.localScale * (1.005f);
+        }
+        if (collision.gameObject.CompareTag("GroundIce") && transform.localScale.x >= minSize)
+        {
+            transform.localScale = transform.localScale * (0.999f);
         }
     }
 }
