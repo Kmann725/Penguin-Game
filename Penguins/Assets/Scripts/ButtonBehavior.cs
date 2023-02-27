adb70473a53d55565c70a1f7f7ed1c4ca9f7831e
Kmann725 <khmanning@mail.bradley.edu>
2023-02-27 12:10:10 -0600

    Added main menu buttons

---
+++ Penguins/Assets/Scripts/ButtonBehavior.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("IceArea");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
